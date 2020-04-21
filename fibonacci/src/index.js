67d7791dae4d55ad42a914a5ffedb51ee4f8bd7c
Joao C Barbosa <joaao@outlook.com>
2020-04-21 18:29:08 -0300

    Third module - Fibonacci sequence with Node JS

--- fibonacci/src/index.js
+++ fibonacci/src/index.js
@@ -1,25 +1,25 @@
 const fibonacci = () => {
     let initial = 0;
-    const fibonacci_array = [0]
+    const fibonacci_array = [0];
 
     while (initial <= 350) {
         if (initial === 0) {
-            fibonacci_array.push(1)
-            initial = 1
+            fibonacci_array.push(1);
+            initial = 1;
         } else {
-            const elementToAdd = fibonacci_array[fibonacci_array.length - 1] + fibonacci_array[fibonacci_array.length - 2]
-            fibonacci_array.push(elementToAdd)
-            initial = elementToAdd
+            const elementToAdd = fibonacci_array[fibonacci_array.length - 1] + fibonacci_array[fibonacci_array.length - 2];
+            fibonacci_array.push(elementToAdd);
+            initial = elementToAdd;
         }
 
     }
-    return fibonacci_array
+    return fibonacci_array;
 }
 
 const isFibonacci = (number) => {
-    const isInArray = fibonacci().includes(number)
+    const isInArray = fibonacci().includes(number);
 
-    return isInArray
+    return isInArray;
 }
 
 module.exports = {
